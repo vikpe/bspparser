436b0c731be29ff9d21e1d259c635624b234b1cd
Viktor Persson <viktor.persson@arcsin.se>
2024-06-25 09:28:48 +0200

    Add benchmark.

---
+++ benches/bench_lib.rs
@@ -0,0 +1,22 @@
+use std::fs;
+
+use criterion::{criterion_group, criterion_main, Criterion, Throughput};
+
+fn get_demo_data() -> Vec<u8> {
+    fs::read("tests/files/dm3_gpl.bsp").expect("unable to read map")
+}
+
+fn lib_benchmark(c: &mut Criterion) {
+    let data = get_demo_data();
+    let mut group = c.benchmark_group("lib");
+    group.throughput(Throughput::Bytes(data.len() as u64));
+
+    group.bench_function("entities::get_entities", |b| {
+        b.iter(|| bspparser::entities::get_entities(&data))
+    });
+
+    group.finish();
+}
+
+criterion_group!(benches, lib_benchmark);
+criterion_main!(benches);
