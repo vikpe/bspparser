47b128284080681e3920335950811e08fe1027a4
Viktor Persson <viktor.persson@arcsin.se>
2025-01-29 15:57:51 +0100

    Misc refactoring.

--- src/bsp.rs
+++ src/bsp.rs
@@ -1,14 +1,13 @@
 // specification: https://www.gamers.org/dEngine/quake/spec/quake-spec34/qkspec_4.htm
 // original: https://github.com/Thinkofname/rust-quake/blob/master/src/bsp/mod.rs
 use crate::parse::*;
-use anyhow::{anyhow as e, Result};
-use bstr::ByteSlice;
+use anyhow::{anyhow as e, Error, Result};
 use cgmath::Vector3;
 use std::collections::HashMap;
+use std::fmt::Display;
 use std::io::{Read, Seek, SeekFrom};
 use std::ops::Range;
 
-pub(crate) const BSP_VERSION: i32 = 29;
 const SIZE_TEXTURE_INFO: usize = 4 * 6 + 4 * 2 + 4 * 2;
 const SIZE_VERTEX: usize = 4 * 3;
 const SIZE_EDGE: usize = 2 + 2;
@@ -16,17 +15,18 @@ const SIZE_PLANE: usize = 4 * 3 + 4 + 4;
 const SIZE_FACE: usize = 2 + 2 + 4 + 2 + 2 + 4 + 4;
 const SIZE_MODEL: usize = (4 * 3) * 3 + 4 * 4 + 4 + 4 + 4;
 
-#[derive(Debug, Default, PartialEq)]
+#[derive(Debug, PartialEq)]
 pub struct BspFile {
+    pub version: Version,
+    pub edge_list: Vec<i32>,
+    pub edges: Vec<Edge>,
     pub entities: Vec<HashMap<String, String>>,
-    pub planes: Vec<Plane>,
-    pub textures: Vec<Texture>,
-    pub texture_info: Vec<TextureInfo>,
     pub faces: Vec<Face>,
     pub light_maps: Vec<u8>,
-    pub edges: Vec<Edge>,
-    pub edge_list: Vec<i32>,
     pub models: Vec<Model>,
+    pub planes: Vec<Plane>,
+    pub texture_info: Vec<TextureInfo>,
+    pub textures: Vec<Texture>,
 }
 
 impl BspFile {
@@ -34,12 +34,12 @@ impl BspFile {
     where
         R: Read + Seek,
     {
-        // 1. Model version
-        let version = r.read_long()?;
-
-        if version != BSP_VERSION {
-            return Err(e!("Unsupported BSP version, must be {BSP_VERSION}"));
-        }
+        // 1. BSP version
+        let version = {
+            let mut version_bytes = [0; 4];
+            r.read_exact(&mut version_bytes)?;
+            Version::try_from(version_bytes)?
+        };
 
         let e_entities = Entry::read(r)?;
         let e_planes = Entry::read(r)?;
@@ -58,6 +58,7 @@ impl BspFile {
         let e_models = Entry::read(r)?;
 
         // 2. Entities
+        println!("2. Entities");
         let entities = {
             let mut entities_buf = vec![0; e_entities.size as usize];
             r.seek(SeekFrom::Start(e_entities.offset as u64))?;
@@ -66,10 +67,12 @@ impl BspFile {
         }?;
 
         // 3. Planes
+        println!("3. Planes");
         r.seek(SeekFrom::Start(e_planes.offset as u64))?;
         let planes = Plane::parse(e_planes.size as usize / SIZE_PLANE, r)?;
 
         // 4. Wall Textures
+        println!("4. Wall Textures");
         r.seek(SeekFrom::Start(e_wall_textures.offset as u64))?;
         let textures = Texture::parse(r)?;
 
@@ -121,6 +124,7 @@ impl BspFile {
 
         // Done!
         Ok(BspFile {
+            version,
             entities,
             light_maps,
             textures,
@@ -135,7 +139,7 @@ impl BspFile {
 }
 
 fn parse_entities(bytes: &[u8]) -> Result<Vec<HashMap<String, String>>> {
-    let entities_str = bytes.to_str()?.to_string();
+    let entities_str = quake_text::bytestr::to_unicode(bytes);
     let mut entities = Vec::new();
     let mut current_entity = HashMap::new();
 
@@ -155,6 +159,33 @@ fn parse_entities(bytes: &[u8]) -> Result<Vec<HashMap<String, String>>> {
         }
     }
     Ok(entities)
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Version {
+    V29,
+    BSP2,
+}
+
+impl Display for Version {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Version::V29 => write!(f, "29"),
+            Version::BSP2 => write!(f, "BSP2"),
+        }
+    }
+}
+
+impl TryFrom<[u8; 4]> for Version {
+    type Error = Error;
+
+    fn try_from(version: [u8; 4]) -> Result<Self, Self::Error> {
+        match version {
+            [29, 0, 0, 0] => Ok(Version::V29),
+            [66, 83, 80, 50] => Ok(Version::BSP2),
+            _ => Err(e!("Unsupported BSP version")),
+        }
+    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -287,7 +318,7 @@ pub struct TextureInfo {
     pub vector_t: Vector3<f32>,
     pub dist_t: f32,
     pub texture_index: usize,
-    pub animated: bool,
+    pub is_animated: bool,
 }
 
 impl TextureInfo {
@@ -298,20 +329,13 @@ impl TextureInfo {
         let mut info = Vec::with_capacity(count);
 
         for _ in 0..count {
-            let vector_s = Vector3::from(r.read_vector3_float()?);
-            let dist_s = r.read_float()?;
-            let vector_t = Vector3::from(r.read_vector3_float()?);
-            let dist_t = r.read_float()?;
-            let texture_index = r.read_ulong()? as usize;
-            let animated = r.read_ulong()? != 0;
-
             info.push(TextureInfo {
-                vector_s,
-                dist_s,
-                vector_t,
-                dist_t,
-                texture_index,
-                animated,
+                vector_s: Vector3::from(r.read_vector3_float()?),
+                dist_s: r.read_float()?,
+                vector_t: Vector3::from(r.read_vector3_float()?),
+                dist_t: r.read_float()?,
+                texture_index: r.read_ulong()? as usize,
+                is_animated: r.read_ulong()? != 0,
             })
         }
 
@@ -427,7 +451,7 @@ mod tests {
     use std::fs;
 
     #[test]
-    fn test_parse() -> Result<()> {
+    fn test_parse_v29() -> Result<()> {
         {
             let file = &mut fs::File::open("tests/files/povdmm4.bsp")?;
             let bsp = BspFile::parse(file)?;
@@ -437,13 +461,13 @@ mod tests {
                 bsp.entities.first(),
                 Some(&HashMap::from([
                     ("classname".to_string(), "worldspawn".to_string()),
-                    ("worldtype".to_string(), "1".to_string()),
-                    ("sounds".to_string(), "0".to_string()),
                     (
                         "message".to_string(),
                         "DMM4 Arena\\nBy Povo-Hat (http://povo-hat.besmella-quake.com)\\n"
                             .to_string()
                     ),
+                    ("sounds".to_string(), "0".to_string()),
+                    ("worldtype".to_string(), "1".to_string()),
                 ]))
             );
             assert_eq!(
@@ -454,14 +478,47 @@ mod tests {
                 ]))
             );
 
-            assert_eq!(bsp.edges.len(), 760);
-            assert_eq!(bsp.light_maps.len(), 15850);
-            assert_eq!(bsp.textures.len(), 8);
-            assert_eq!(bsp.texture_info.len(), 21);
             assert_eq!(bsp.edge_list.len(), 1518);
-            assert_eq!(bsp.planes.len(), 191);
+            assert_eq!(bsp.edges.len(), 760);
             assert_eq!(bsp.faces.len(), 323);
+            assert_eq!(bsp.light_maps.len(), 15850);
             assert_eq!(bsp.models.len(), 5);
+            assert_eq!(bsp.planes.len(), 191);
+            assert_eq!(bsp.texture_info.len(), 21);
+            assert_eq!(bsp.textures.len(), 8);
+        }
+        {
+            let file = &mut fs::File::open("tests/files/dm3_gpl.bsp")?;
+            let bsp = BspFile::parse(file)?;
+
+            assert_eq!(bsp.entities.len(), 211);
+            assert_eq!(
+                bsp.entities.first(),
+                Some(&HashMap::from([
+                    ("classname".to_string(), "worldspawn".to_string()),
+                    ("message".to_string(), "The Abandoned Base".to_string()),
+                    ("sounds".to_string(), "6".to_string()),
+                    ("wad".to_string(), "gfx/base.wad".to_string()),
+                    ("worldtype".to_string(), "2".to_string()),
+                ]))
+            );
+            assert_eq!(
+                bsp.entities.last(),
+                Some(&HashMap::from([
+                    ("classname".to_string(), "info_intermission".to_string()),
+                    ("mangle".to_string(), "20 240 0".to_string()),
+                    ("origin".to_string(), "1840 256 64".to_string()),
+                ]))
+            );
+
+            assert_eq!(bsp.edge_list.len(), 16002);
+            assert_eq!(bsp.edges.len(), 8030);
+            assert_eq!(bsp.faces.len(), 3236);
+            assert_eq!(bsp.light_maps.len(), 134639);
+            assert_eq!(bsp.models.len(), 7);
+            assert_eq!(bsp.planes.len(), 835);
+            assert_eq!(bsp.texture_info.len(), 272);
+            assert_eq!(bsp.textures.len(), 59);
         }
         Ok(())
     }
